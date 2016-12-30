4dcbe77829d83e8983666b8d1ad41b0aa4876e6c
Neon Chen <neon_chen@tw.shuttle.com>
2016-12-30 13:48:04 +0800

    Add [API]: Server Connection

--- api/connection.py
+++ api/connection.py
@@ -12,18 +12,28 @@ fail_message = {'Message':'xxxxxxxxxxxxxxxx'}
 def check(request):
     if request.method == 'POST':
         try:
-            """response note
-            enter code
+            client_ip = request.META['REMOTE_ADDR']
+            req = json.loads(request.body.decode('utf-8'))
+            req_key = {'IP'}
+            if all(k in req for k in req_key) and all(req.values()):
+                try:
+                    from api.models import Devices
+                    devices = Devices.objects.get(ip = client_ip)
 
-            you can refer to the following method:
-            if success, enter values to below variables:
-            result
-            then user like below code to response:
-            res = {'Result' : result}
-
-            if error, use like below code to response:
-            res = dict(ChainMap(result_fail, fail_code999, fail_message))
-            """
+                    if devices.state == 'Proposed':
+                        res = dict({'Result':0})
+                    elif devices.state == 'Approved':
+                        res = dict({'Result':1})
+                    elif devices.state == 'Setting':
+                        res = dict({'Result':2})
+                    elif devices.state == 'Getting':
+                        res = dict({'Result':3})
+                    else:
+                        res = dict(ChainMap(result_fail, fail_code999, fail_message))
+                except:
+                    res = dict(ChainMap(result_fail, fail_code999, fail_message))
+            else:
+                res = dict(ChainMap(result_fail, fail_code999, fail_message))
         except:
             res = dict(ChainMap(result_fail, fail_code999, fail_message))
 
@@ -34,27 +44,28 @@ def check(request):
 def get(request):
     if request.method == 'POST':
         try:
-            """response note
-            enter code
+            client_ip = request.META['REMOTE_ADDR']
+            req = json.loads(request.body.decode('utf-8'))
+            req_key = {'IP'}
+            if all(k in req for k in req_key) and all(req.values()):
+                try:
+                    from api.models import Devices
+                    devices = Devices.objects.get(ip = client_ip)
 
-            you can refer to the following method:
-            if success, enter values to below variables:
-            welcome, accessdenied, screensaver, ntp, timezone, audio, bioscore, language
-            then user like below code to response:
-            res = dict(ChainMap(result_success, {
-                'Welcome' : welcome,
-                'AccessDenied' : accessdenied,
-                'ScreenSaver' : screensaver,
-                'NTP' : ntp,
-                'Timezone' : timezone,
-                'Audio' : audio,
-                'BioScore' : bioscore,
-                'Language' : language
-            }))
-
-            if error, use like below code to response:
-            res = dict(ChainMap(result_fail, fail_code999, fail_message))
-            """
+                    res = dict(ChainMap(result_success, {
+                        'Welcome' : devices.welcome,
+                        'AccessDenied' : devices.accessdenied,
+                        'ScreenSaver' : devices.screensaver,
+                        'NTP' : devices.ntp,
+                        'Timezone' : devices.timezone,
+                        'Audio' : devices.audio,
+                        'BioScore' : devices.bioscore,
+                        'Language' : devices.language
+                    }))
+                except:
+                    res = dict(ChainMap(result_fail, fail_code999, fail_message))
+            else:
+                res = dict(ChainMap(result_fail, fail_code999, fail_message))
         except:
             res = dict(ChainMap(result_fail, fail_code999, fail_message))
 
@@ -65,13 +76,24 @@ def get(request):
 def set(request):
     if request.method == 'POST':
         try:
+            client_ip = request.META['REMOTE_ADDR']
             req = json.loads(request.body.decode('utf-8'))
             req_key = {'Welcome', 'AccessDenied', 'ScreenSaver', 'NTP', 'Timezone', 'Audio', 'BioScore', 'Language'}
             if all(k in req for k in req_key) and all(req.values()):
-                """
-                enter code
-                """
-                res = dict(result_success)
+                try:
+                    from api.models import Devices
+                    Devices.objects.filter(ip = client_ip).update(welcome = req['Welcome'])
+                    Devices.objects.filter(ip = client_ip).update(accessdenied = req['AccessDenied'])
+                    Devices.objects.filter(ip = client_ip).update(screensaver = req['ScreenSaver'])
+                    Devices.objects.filter(ip = client_ip).update(ntp = req['NTP'])
+                    Devices.objects.filter(ip = client_ip).update(timezone = req['Timezone'])
+                    Devices.objects.filter(ip = client_ip).update(audio = req['Audio'])
+                    Devices.objects.filter(ip = client_ip).update(bioscore = req['BioScore'])
+                    Devices.objects.filter(ip = client_ip).update(language = req['Language'])
+
+                    res = dict(result_success)
+                except:
+                    res = dict(ChainMap(result_fail, fail_code999, fail_message))
             else:
                 res = dict(ChainMap(result_fail, fail_code999, fail_message))
         except:
